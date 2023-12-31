c8318cdab587dc2f52cf30dff840558b305d4ab4
Samat Galimov <s@samat.me>
2023-12-31 10:22:06 +0200

    version 2

--- goodsecretarybot.py
+++ goodsecretarybot.py
@@ -1,37 +1,41 @@
-import os
+from telegram import Update
+from telegram.ext import Application, CommandHandler, MessageHandler, filters, CallbackContext
 import requests
-from telegram.ext import Updater, CommandHandler, MessageHandler, filters
+import os
 
-def start(update, context):
-    update.message.reply_text('Hi! I am a Good Secretary Bot. Send me your voice message and I will transcribe them for you using OpenAI Whisper API. Messages are not stored and not logged in this bot at any point.')
+async def start(update: Update, context: CallbackContext) -> None:
+    await update.message.reply_text('Hi! Send me a voice message.')
 
-def transcribe_voice(update, context):
-    voice_file = update.message.voice.get_file()
+async def transcribe_voice(update: Update, context: CallbackContext) -> None:
+    voice_file = await context.bot.get_file(update.message.voice.file_id)
     voice_file.download('voice.ogg')
 
-    # Send the file to OpenAI's Whisper API
     with open('voice.ogg', 'rb') as f:
         response = requests.post(
-            "https://api.openai.com/v1/whisper/transcribe",
-            headers={"Authorization": f"Bearer ***REMOVED***"},
+            "YOUR_WHISPER_API_ENDPOINT",  # Replace with the actual Whisper API endpoint
+            headers={"Authorization": "Bearer ***REMOVED***"},
             files={"file": f}
         )
 
     if response.status_code == 200:
         transcription = response.json().get("transcription")
-        update.message.reply_text(transcription)
+        await update.message.reply_text(transcription)
     else:
-        update.message.reply_text("Sorry, I couldn't transcribe that.")
+        await update.message.reply_text("Sorry, I couldn't transcribe that.")
 
     os.remove('voice.ogg')
 
-def main():
-    updater = Updater("***REMOVED***")
-    dp = updater.dispatcher
-    dp.add_handler(CommandHandler("start", start))
-    dp.add_handler(MessageHandler(Filters.voice, transcribe_voice))
-    updater.start_polling()
-    updater.idle()
+async def main():
+    application = Application.builder().token("***REMOVED***").build()
+
+    start_handler = CommandHandler('start', start)
+    voice_handler = MessageHandler(filters.VOICE, transcribe_voice)
+
+    application.add_handler(start_handler)
+    application.add_handler(voice_handler)
+
+    await application.run_polling()
 
 if __name__ == '__main__':
-    main()
+    import asyncio
+    asyncio.run(main())
